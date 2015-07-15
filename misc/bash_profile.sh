eed33bd681843edceddef413b15614f81cf33573
Mike Dunn <mike@eikonomega.com>
2015-07-14 21:41:30 -0400

    Add aliases to bash_profile to allow students to easily navigate to class folders.

--- misc/bash_profile.sh
+++ misc/bash_profile.sh
@@ -8,7 +8,7 @@ echo "alias dragonwarrior='cd /vagrant/trainee-area'" >> /home/vagrant/.bash_pro
 echo "alias level-0='cd /vagrant/trainee-area/level-0-example'" >> /home/vagrant/.bash_profile
 echo "alias level-1='cd /vagrant/trainee-area/level-1'" >> /home/vagrant/.bash_profile
 echo "alias level-2='cd /vagrant/trainee-area/level-2'" >> /home/vagrant/.bash_profile
-echo "alias level-3='cd /vagrant/trainee-area/level-3-creating-web-services'" >> /home/vagrant/.bash_profile
+echo "alias level-3='cd /vagrant/trainee-area/level-3-creating-web-services/trainee-friends-api'" >> /home/vagrant/.bash_profile
 echo "alias level-4='cd /vagrant/trainee-area/level-4'" >> /home/vagrant/.bash_profile
 echo "alias level-5='cd /vagrant/trainee-area/level-5'" >> /home/vagrant/.bash_profile
 
