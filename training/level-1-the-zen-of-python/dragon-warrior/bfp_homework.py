e55c6841e692bddc35bae7a2bb9f0265029d8e31
BFP <mike@eikonomega.com>
2015-10-19 18:54:36 +0000

    Add my homework.

---
+++ training/level-1-the-zen-of-python/dragon-warrior/bfp_homework.py
@@ -0,0 +1,27 @@
+test_list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+test_tuple = (1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+test_string = "Example String"
+test_range = range(100)
+
+print(list(test_range[0:5:2]))
+
+test_list_of_strings = [
+    "FindMe1",
+    "junk",
+    "FindMe2",
+    "junk",
+    "FindMe3",
+    "junk",
+    "FindMe4",
+    "junk",
+    "FindMe5",
+    "junk",
+    "FindMe6",
+    "junk",
+    "FindMe7"
+]
+
+for item in test_range:
+
+
+
