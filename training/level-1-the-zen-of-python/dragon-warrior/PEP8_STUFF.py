fc6252bd2b850cf6be21aa1ef790645902b27a91
Ryan Knowlton <ryan.knowlton@nd.edu>
2015-10-19 14:56:21 -0400

    Add homework file

--- training/level-1-the-zen-of-python/dragon-warrior/PEP8_STUFF.py
+++ training/level-1-the-zen-of-python/dragon-warrior/PEP8_STUFF.py
@@ -1,27 +1,33 @@
 """
-This code (which finds the sume of all unique multiples of 3/5 within a
+This code (which finds the sum of all unique multiples of 3/5 within a
 given limit) breaks a significant number of PEP8 rules.  Maybe it doesn't even
 work at all.
 
 Find and fix all the PEP8 errors.
 """
-import os, sys, operator
+import os
+import sys
+import operator
 
-INTERESTEDMULTIPLES = [3, 5]
+INTERESTED_MULTIPLES = [3, 5]
 
-def quickerMultipleOfThreeAndFive(top_limit):
+
+def quicker_multiple_of_three_and_five(top_limit):
     multiplier = 1
-    multiples  = set()
+    multiples = set()
 
     while True:
-        if multiplier * 5 < top_limit: multiples.add(multiplier * 5)
+        if multiplier * 5 < top_limit:
+            multiples.add(multiplier * 5)
         if multiplier * 3 < top_limit:
             multiples.add(multiplier * 3)
         else:
-        break
+            break
 
         multiplier += 1
 
     return sum(multiples)
 
-print("The sum of all unique multiples of 3 and 5 which given an upper limit of X is to consider is: answer")
+
+print("The sum of all unique multiples of 3 and 5 which given an "
+      "upper limit of X is to consider is: answer")
