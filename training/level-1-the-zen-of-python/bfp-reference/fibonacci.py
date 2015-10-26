a24c4bee679c2dbf24494c73098fc4fa329b6c38
eikonomega <mike@eikonomega.com>
2015-10-26 12:48:41 -0400

    Add example files.

--- training/level-1-the-zen-of-python/bfp-reference/fibonacci.py
+++ training/level-1-the-zen-of-python/bfp-reference/fibonacci.py
@@ -9,6 +9,7 @@ exceed four million, find the sum of the even-valued terms.
 """
 
 
+# Brute Force, with Generators and In-Place Multiple Variable Reassignment
 def fibonacci(max_number):
     current_number = 1
     last_number = 0
@@ -16,7 +17,6 @@ def fibonacci(max_number):
     while current_number <= max_number:
         last_number, current_number = (current_number,
                                        current_number + last_number)
-
 
         if not current_number % 2:
             yield current_number
--- training/level-2-command-line-interfaces/bfp-reference/exercise_1.py
+++ training/level-2-command-line-interfaces/bfp-reference/exercise_1.py
@@ -1,4 +1,8 @@
 """
 This module provides a CLI that...
 """
+import sys
 import argparse  # Imports should appear immediately after module docstrings.
+
+print(sys.modules)
+print(sys.path)
