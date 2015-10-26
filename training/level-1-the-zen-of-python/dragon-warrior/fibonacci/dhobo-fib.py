5155e6808926d32defe96a5bfa9236e8249bc1d1
Dirty-Hobo <jamessm010@gmail.com>
2015-10-26 19:01:53 +0000

    Pandas Stink

---
+++ training/level-1-the-zen-of-python/dragon-warrior/fibonacci/dhobo-fib.py
@@ -0,0 +1,18 @@
+__author__ = 'jsmith68'
+## Generate list of Fib Numbers
+
+def fib():
+    a,b = 0,1
+    while True:
+        yield a
+        a,b = b, a + b
+
+fibList = []
+for index,fib_number in enumerate(fib()):
+    if fib_number%2 == 0:
+        fibList.append(fib_number)
+    if fib_number >= 4000000:
+            break
+
+#print(str(fibList).strip('[]'))
+print(str(sum(fibList)))
