27c19b51409c3203562c1538f62e933b2dddce56
Dirty-Hobo <jamessm010@gmail.com>
2015-10-26 19:14:05 +0000

    Fib Fix

--- training/level-1-the-zen-of-python/dragon-warrior/fibonacci/dhobo-fib2.py
+++ training/level-1-the-zen-of-python/dragon-warrior/fibonacci/dhobo-fib2.py
@@ -1,8 +1,8 @@
 __author__ = 'jsmith68'
-sum = 0
+total = 0
 f1, f2 = 0, 1
 while f2 < 4000000:
     if f2 % 2 == 0:
-        sum+= f2
-    f1,f2 = f2, f1 + f2
-print (sum)
+        total += f2
+    f1, f2 = f2, f1 + f2
+print (total)
