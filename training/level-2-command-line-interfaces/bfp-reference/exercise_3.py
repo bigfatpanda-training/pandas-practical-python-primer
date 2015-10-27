20cbc2f2d94ca3dc4e64555b59c923e181f232ce
eikonomega <mike@eikonomega.com>
2015-10-27 18:28:24 -0400

    New class materials.

--- training/level-2-command-line-interfaces/bfp-reference/exercise_3.py
+++ training/level-2-command-line-interfaces/bfp-reference/exercise_3.py
@@ -8,12 +8,11 @@ parser = argparse.ArgumentParser(
     description="My cool program does a lot of cool things.",
     epilog="Thanks for using my cool program")
 
-parser.add_argument("--filenames", nargs="+", metavar="FILENAME",
-                    required=True, help="Names of files to copy.")
+parser.add_argument("filenames", nargs="+", metavar="FILENAME",
+                    help="Names of files to copy.")
 
-parser.add_argument('--destination', required=True,
-                    dest='destination', help='Location to copy files to.')
-
+# What's the problem with this going to be?
+parser.add_argument('destination', help='Location to copy files to.')
 
 program_arguments = parser.parse_args()
 print(program_arguments)
