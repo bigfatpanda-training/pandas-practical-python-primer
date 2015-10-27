99c67cadc8601795e6ac96efe6b82618edc18db1
rwharris-nd <rickharris724@gmail.com>
2015-10-27 19:05:11 +0000

    Add my parseargs homework.

---
+++ training/level-2-command-line-interfaces/dragon-warrior/rwharris-nd_stdlib_cli.py
@@ -0,0 +1,19 @@
+"""
+This module provides a CLI that...
+"""
+
+import argparse
+
+parser = argparse.ArgumentParser(
+    prog="my_program",
+    description="This is a useful description.",
+    epilog="This is epic."
+)
+
+parser.add_argument("filenames",nargs="+",metavar="FILENAME",
+                    help="Name of file(s) to copy.")
+
+parser.add_argument("-d",dest='destination',metavar="DESTINATION",
+                    required=True,type=str,help="Destination file path.")
+
+print(parser.parse_args())
