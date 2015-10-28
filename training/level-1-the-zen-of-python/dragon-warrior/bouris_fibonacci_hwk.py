681502998be322cc6e2d50b079c96c5aa45cdaea
eikonomega <mike@eikonomega.com>
2015-10-28 11:14:37 -0400

    Move homework assignments to correct locations.

--- training/level-1-the-zen-of-python/dragon-warrior/bouris_fibonacci_hwk.py
+++
@@ -1,7 +0,0 @@
-__author__ = 'pbouris'
-
-first_num, second_num = 0, 1
-print (first_num)
-while second_num <= 4000000:
-    print (second_num)
-    first_num, second_num = second_num, first_num + second_num
