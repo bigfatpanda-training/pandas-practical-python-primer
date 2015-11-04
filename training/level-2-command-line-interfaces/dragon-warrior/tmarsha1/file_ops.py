3e26d15eda495391892f6a9697f6baefdc1e0eea
Troy L. Marshall <tmarsha1@nd.edu>
2015-11-04 14:35:47 -0500

    Add Monday's class changes

--- training/level-2-command-line-interfaces/dragon-warrior/tmarsha1/file_ops.py
+++ training/level-2-command-line-interfaces/dragon-warrior/tmarsha1/file_ops.py
@@ -1,4 +1,10 @@
-"""This module provides various functions for operating on files"""
+"""
+This module provides various functions for operating on files
+
+Functions:
+    copy_files: copy file(s) to a specific location.
+
+"""
 
 import subprocess
 
@@ -13,9 +19,27 @@ def copy_files(files: list, destination: str):
     """
 
     for file in files:
-        byteresult = subprocess.check_output(
-            args=['cp', '-vp', file, destination],
-            stderr=subprocess.STDOUT)
-        result = byteresult.decode()
-        result = result. rstrip()
-        print(result)
+        try:
+            byte_result = subprocess.check_output(
+                args=['cp', '-vp', file, destination],
+                stderr=subprocess.STDOUT)
+        except subprocess.CalledProcessError as file_error:
+            message = file_error.output.decode()
+
+            if "cannot stat" in message:
+                print("ERROR: '{}' does not exist.".format(file))
+            elif "failed to access" in message:
+#                print(message)
+                print("ERROR: '{}' destination does not exist.".format(destination))
+            elif "Permission" in message:
+                print("ERROR: Insufficient rights to destination '{}'.".format(destination))
+        else:
+            result = byte_result.decode()
+            result = result. rstrip()
+            print(result)
+        finally:
+            pass
+
+
+def move_files(filenames, destination):
+    return None
