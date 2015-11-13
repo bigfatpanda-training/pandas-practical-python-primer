2a07554d478b1a3191f5ffd5785cb047dfabe663
Ryan Knowlton <ryan.knowlton@nd.edu>
2015-11-13 14:09:04 -0500

    Update stdlib_cli homework

--- training/level-2-command-line-interfaces/dragon-warrior/ndrk/file_ops.py
+++ training/level-2-command-line-interfaces/dragon-warrior/ndrk/file_ops.py
@@ -6,7 +6,7 @@ Functions:
 
 """
 
-import subprocess
+import shutil
 
 
 def copy_files(files: list, destination: str):
@@ -20,21 +20,54 @@ def copy_files(files: list, destination: str):
 
     for file in files:
         try:
-            result = subprocess.check_output(
-                args=['cp', '-vp', file, destination],
-                stderr=subprocess.STDOUT
-            )
+            shutil.copy2(file, destination)
+        except shutil.Error as error:
+            print('Error: %s' % error)
+        except FileNotFoundError as error:
+            print('Error: %s' % error)
+        except PermissionError as error:
+            print('Error: %s' % error)
+        except IsADirectoryError as error:
+            print('Error: %s' % error)
+        else:
+            print("Copying " + file)
+        finally:
+            pass
 
-            print(
-                "Copying " + result.decode('utf-8').strip())
-        except subprocess.CalledProcessError as error:
-            exception_msg = error.output.decode()
 
-            if "cannot stat" in exception_msg:
-                print("ERROR: '{}' doesn't exist.".format(file))
-            elif "Not a directory" in exception_msg:
-                print("ERROR: '{}' doesn't exist.".format(destination))
-            elif "Permission denied" in exception_msg:
-                print("ERROR: You don't have permission to '{}'.".format(destination))
-            else:
-                print(error.output.decode())
+def move_files(files: list, destination: str):
+    """
+    Move files to a given destination.
+
+    Args:
+        files: A list of files to move.
+        destination: A string specifying the destination to move the files to.
+    """
+
+    for file in files:
+        try:
+            shutil.move(file, destination)
+        except shutil.Error as error:
+            print('Error: %s' % error)
+        except FileNotFoundError as error:
+            print('Error: %s' % error)
+        except PermissionError as error:
+            print('Error: %s' % error)
+        except IsADirectoryError as error:
+            print('Error: %s' % error)
+        else:
+            print("Moving " + file)
+        finally:
+            pass
+
+
+def rename_file(file: str, destination: str):
+    """
+    Rename file.
+
+    Args:
+        file: A file to rename.
+        destination: A string specifying the destination to rename the files to.
+    """
+
+    move_files([file], destination)
