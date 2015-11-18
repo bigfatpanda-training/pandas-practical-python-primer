ecc59b4e885f5f5b7900d018c7e4174035abf759
eikonomega <mike@eikonomega.com>
2015-11-18 13:25:13 -0500

    Add code files for exercises 06-08

--- training/level-3-interacting-with-web-services/bfp-reference/exercise_06/github_api.py
+++ training/level-3-interacting-with-web-services/bfp-reference/exercise_06/github_api.py
@@ -10,77 +10,92 @@ from configuration_panda import ConfigurationPanda
 credentials = ConfigurationPanda(['PROGRAM_CREDENTIALS'])
 
 
+def github_entry_point() -> requests.Response:
+    """
+    HTTP GET `https://api.github.com`
+
+    Obtain information about the Github API from its entry point.
+    """
+    url = "https://api.github.com"
+    response = requests.get(url)
+    return response
+
+
+def github_user_info(username: str) -> requests.Response:
+    """
+    Obtain information about a given user in Github.
+
+    Args
+        username: A str specifying a valid Github username.
+
+    Returns
+        A requests.Response object.
+    """
+    github_api_info = github_entry_point().json()
+    url = github_api_info['user_url'].format(user=username)
+    response = requests.get(url)
+    return response
+
+
+def github_user_repos(username: str) -> requests.Response:
+    """
+    Obtain information about a given user's repositories in Github.
+
+    Args
+        username: A str specifying a valid Github username.
+
+    Returns
+        A requests.Response object.
+    """
+    github_api_info = github_entry_point().json()
+    url = github_api_info['user_url'].format(user=username) + "/repos"
+    response = requests.get(url)
+    return response
+
+
+def github_repo_issues(username: str, repo_name: str) -> requests.Response:
+    """
+    Obtain information about a given repositories issues in Github.
+
+    Args
+        username: A str specifying a valid Github username.
+        repo_name: A str specifying a Github repo that belongs to the
+            specified user.
+
+    Returns
+        A requests.Response object.
+    """
+    github_api_info = github_entry_point().json()
+    url = (
+        github_api_info['repository_url'].format(
+            owner=username, repo=repo_name) + "/issues")
+    print(url)
+    response = requests.get(url)
+    return response
+
+
 class Github:
     """
     Provides access to the Github API.
 
-    Attributes:
+    Class Attributes:
         urls: Github API url templates for accessing various functionality.
+
+    Attributes:
+        oauth_token: A valid OAuth oauth_token for access the API.
 
     Methods:
         user_info: Provide information on a given Github user.
         user_repos: Provide information on a given users repositories.
         repo_issues: Provide information on a given repo's issues.
     """
+    urls = requests.get("https://api.github.com").json()
 
-    def __init__(self):
-        self.urls = requests.get("https://api.github.com").json()
-
-    def user_info(self, username: str) -> requests.Response:
-        """
-        Obtain information about a given user in Github.
-
-        Args
-            username: A str specifying a valid Github username.
-
-        Returns
-            A requests.Response object.
-        """
-        url = self.urls['user_url'].format(user=username)
-        return requests.get(url)
-
-    def user_repos(self, username: str) -> requests.Response:
-        """
-        Obtain information about a given user's repositories in Github.
-
-        Args
-            username: A str specifying a valid Github username.
-
-        Returns
-            A requests.Response object.
-        """
-        url = self.urls['user_url'].format(user=username) + "/repos"
-        return requests.get(url)
-
-    def repo_issues(self, username: str, repo_name: str) -> requests.Response:
-        """
-        Obtain information about a given repositories issues in Github.
-
-        Args
-            username: A str specifying a valid Github username.
-            repo_name: A str specifying a Github repo that belongs to the
-                specified user.
-
-        Returns
-            A requests.Response object.
-        """
-        url = (
-            self.urls['repository_url'].format(
-                owner=username, repo=repo_name) + "/issues")
-        return requests.get(url)
+    def __init__(self, oauth_token: str):
+        self.oauth_token = oauth_token
 
 
 if __name__ == "__main__":
-    import pprint
-    github = Github()
-
-    user_info = github.user_info('bigfatpanda-training')
-    user_repos = github.user_repos('bigfatpanda-training')
-    bigfatpanda_issues = github.repo_issues(
-        username='bigfatpanda-training',
-        repo_name='pandas-practical-python-primer')
-
-    pprint.pprint(user_info.json())
-    pprint.pprint(user_repos.json())
-    pprint.pprint(bigfatpanda_issues.json())
+    github_1 = Github(oauth_token='pretend token 1')
+    github_2 = Github(oauth_token='pretend token 2')
 
