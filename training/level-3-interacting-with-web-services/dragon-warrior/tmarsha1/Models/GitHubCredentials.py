48139cd6ba176441033580a0467f7a9f42391c67
Troy L. Marshall <tmarsha1@nd.edu>
2015-11-23 14:37:32 -0500

    Add credentials classes to remove dependencies.

---
+++ training/level-3-interacting-with-web-services/dragon-warrior/tmarsha1/Models/GitHubCredentials.py
@@ -0,0 +1,12 @@
+from configuration_panda import ConfigurationPanda
+from Models.Credentials import Credentials
+
+
+class GitHubCredentials(Credentials):
+    __credentials = ConfigurationPanda(['PROGRAM_CREDENTIALS'])
+
+    def __init__(self):
+        pass
+
+    def tokens(self):
+        return GitHubCredentials.__credentials.tokens["github"]
