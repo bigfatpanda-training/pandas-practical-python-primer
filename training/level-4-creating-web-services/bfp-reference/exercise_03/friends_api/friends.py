939c8d1e1dc23bacb61c6511ff254aed9cda1757
eikonomega <mike@eikonomega.com>
2015-12-02 07:57:14 -0500

    Add code files for exercises 4/5

--- training/level-4-creating-web-services/bfp-reference/exercise_03/friends_api/friends.py
+++ training/level-4-creating-web-services/bfp-reference/exercise_03/friends_api/friends.py
@@ -1,3 +1,5 @@
 from flask import Flask, jsonify
 
+from friends_api import datastore
+
 app = Flask(__name__)
---
+++ training/level-4-creating-web-services/bfp-reference/exercise_05/friends_api/friends.py
@@ -0,0 +1,38 @@
+from flask import Flask, jsonify, make_response
+
+from friends_api import datastore
+
+app = Flask(__name__)
+
+
+@app.route('/api/v1/friends', methods=['GET'])
+def friends():
+    """
+    Return a representation of the collection of friend resources.
+
+    Returns:
+        A flask.Response object.
+    """
+    return jsonify({"friends": datastore.friends})
+
+
+@app.route('/api/v1/friends/<id>', methods=['GET'])
+def specific_friend(id: str):
+    """
+    Return a representation of a specific friend resource.
+
+    Args:
+        id: The unique ID value of a given friend.
+
+    Returns:
+        A flask.Response object.
+    """
+    for friend in datastore.friends:
+        if friend['id'].lower() == id.lower():
+            return jsonify(friend)
+
+    error_response = make_response(
+        jsonify(
+            {"error": "No friend found with the specified identifier. "
+                      "BFP is a Big Fat Panda Loser!"}), 404)
+    return error_response
